6c7c86b02ba625edfc4452a8e407b9d18e2fd250
beowax <beowax@gmail.com>
2019-01-15 14:03:21 +0100

    Revert "Test"
    This reverts commit 5b7de5506154118fa25fe233c4f334c7d278efee.

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,16 @@
 </template>
 
 <script>
-  //import AppHeader from './components/AppHeader.vue'
-  //import AppFooter from './components/AppFooter.vue'
-
+  import AppHeader from './components/AppHeader.vue'
+  import AppFooter from './components/AppFooter.vue'
+  
   
   export default {
-    name: 'App'/*,
+    name: 'App',
     components: {
       AppHeader,
       AppFooter
-    }*/
+    }
   }
 
 </script>
