c68443c894b8621540b2451b3c53dd8d4cab0769
beowax <beowax@gmail.com>
2019-03-26 10:43:30 +0100

    Test

--- src/main.js
+++ src/main.js
@@ -5,12 +5,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import {store} from './store/store'
-
 //Vue.use(Vuex)
-
-
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -19,3 +15,17 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+app.listen(port);
+
+
